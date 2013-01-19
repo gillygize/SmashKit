ae79255389e7a0d2ec6c786cfb59ba6596126300
Matthew Gillingham <me@mattgillingham.com>
2013-01-19 11:46:11 +0900

    Initial Commit

---
+++ SmashKit/TestAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TestAppDelegate.h
+//  SmashKit
+//
+//  Created by Matt on 1/19/13.
+//  Copyright (c) 2013 Matthew Gillingham. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TestAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SmashKit/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SmashKit
+//
+//  Created by Matt on 1/19/13.
+//  Copyright (c) 2013 Matthew Gillingham. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TestAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([TestAppDelegate class]));
+  }
+}
